286ac101ca27702939ac7dea3398928f122a6835
Channith Am <amcnith@gmail.com>
2019-01-12 15:47:05 +0700

    chane theme

--- source/conf.py
+++ source/conf.py
@@ -90,7 +90,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bootstrap'
+#html_theme = 'bootstrap'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -98,14 +99,14 @@ html_theme = 'bootstrap'
 #
 html_theme_options = {
     # Navigation bar title. (Default: ``project`` value)
-    'navbar_title': "រៀនភាសា C",
+    #'navbar_title': "រៀនភាសា C",
     # Fix navigation bar to top of page?
     # Values: "true" (default) or "false"
-    'navbar_fixed_top': "true",
+    #'navbar_fixed_top': "true",
 
     # Choose Bootstrap version.
     # Values: "3" (default) or "2" (in quotes)
-    'bootstrap_version': "3",
+    #'bootstrap_version': "3",
 
 }
 
